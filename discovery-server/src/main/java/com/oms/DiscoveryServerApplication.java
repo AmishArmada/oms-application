dcd368c92cff657ad16b41c0b47975ab3ad54452
Corey Griffith <coreygriff1999@gmail.com>
2022-02-15 11:19:51 -0500

    Changes to discovery server port for ease of use

--- discovery-server/src/main/java/com/oms/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/oms/DiscoveryServerApplication.java
@@ -2,6 +2,7 @@ package com.oms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,5 +11,4 @@ public class DiscoveryServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
-
 }
